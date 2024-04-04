fd0b560fd7afd509e6304f5c0c37c814c49e0ffd
Tran Nguyen Vu <trannguyenvu774411@gmail.com>
2024-04-04 13:11:32 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WEBSAIGONGLISTEN.Models;
+
+namespace WEBSAIGONGLISTEN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // Hien thi danh sach san pham
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
